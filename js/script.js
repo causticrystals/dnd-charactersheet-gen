59c96ead4eb355b591ee2599b0b29d133ac05957
causticrystals <peanutpc173@gmail.com>
2022-11-09 13:11:44 -0700

    index and script updates

--- js/script.js
+++ js/script.js
@@ -8,6 +8,9 @@ document.getElementById("preview-btn").addEventListener("click", previewBtn);
 
 // span variables
 let traitsBonus = document.getElementById("traits-bonus");
+let saves = document.getElementById("saves");
+let hitDie = document.getElementById("hit-die");
+let prAbility = document.getElementById("prime-ability");
 
 // stats variables
 let str = 0;
@@ -33,10 +36,49 @@ function randNameBtn() {
 
 // Change the bonus stats preview
 function previewBtn() {
-    let raceVal = raceSelect.value;
-    let classVal = classSelect.value;
+    let clasSel = classSelect.value;
+    let race = raceSelect.value;
 
-    // calculate race stats
+    // race traits
+    if (race === 'dwarf') {
+        traitsBonus.innerHTML = "+2 Constitution";
+    } else if (race === 'elf') {
+        traitsBonus.innerHTML = "+2 Dexterity";
+    } else if (race === 'half-elf') {
+        traitsBonus.innerHTML = "+2 Charisma & +1 to all other scores";
+    } else if (race === 'human') {
+        traitsBonus.innerHTML = "+1 to all scores";
+    } else if (race === 'dragonborn') {
+        traitsBonus.innerHTML = "+2 Strength & +1 Charisma";
+    } else if (race === 'halfling') {
+        traitsBonus.innerHTML = "+2 Dexterity";
+    } else if (race === 'tiefling') {
+        traitsBonus.innerHTML = "+2 Charisma & +1 Inteligence";
+    }
 
-    // calculate class stats
+    // class saves
+    if (clasSel === 'barbarian') {
+        classBonus("Strength + Constitution", "d12", "Strength");
+    } else if (clasSel === 'bard') {
+        classBonus("Dexterity + Charisma", "d8", "Charisma");
+    } else if (clasSel === 'cleric') {
+        classBonus("Wisdom + Charisma", "d8", "Wisdom");
+    } else if (clasSel === 'druid') {
+        classBonus("Intelgence + Wisdom", "d8", "Wisdom");
+    } else if (clasSel === 'fighter') {
+        classBonus("Strength + Constitution", "d10", "Dexterity");
+    } else if (clasSel === 'paladin') {
+        classBonus("Wisdom + Charisma", "d10", "Strength + Charisma");
+    } else if (clasSel === 'rogue') {
+        classBonus("Dexterity + Inteligence", "d8", "Dexterity");
+    } else if (clasSel === 'wizard') {
+        classBonus("Inteligence + Wisdom", "d6", "Inteligence");
+    }
+}
+
+// Helper function for class traits
+function classBonus(saveTrait, die, ability) {
+    saves.innerHTML = saveTrait;
+    hitDie.innerHTML = die;
+    prAbility.innerHTML = ability;
 }
