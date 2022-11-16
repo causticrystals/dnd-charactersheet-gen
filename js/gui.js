d6ee95a82c717272cdabcc350bad6c4ccc94dd5a
causticrystals <peanutpc173@gmail.com>
2022-11-16 13:28:13 -0700

    hdvbf

---
+++ js/gui.js
@@ -0,0 +1,12 @@
+// HTML variables
+let beginBtn = document.getElementById("begin-btn");
+let beginDiv = document.getElementById("start-div");
+let sheetStatsDiv = document.getElementById("sheet-stats-div");
+
+// begin btn clicked
+beginBtn.addEventListener("click", displayStatsDiv);
+
+function displayStatsDiv() {
+    beginDiv.style.display = "none"
+    sheetStatsDiv.style.display = "block";
+}
